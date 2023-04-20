bc9859a4b383233b471a839d00d3020dcfa8c319
Dann-13 <dannyelgomezz.13@gmail.com>
2023-04-20 15:48:04 -0500

    Limpiando Por defecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listaCompras';
+  title = 'Aplicacion Lista de compras';
 }
